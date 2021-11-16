d3974fa58e77297e76e5f5850d1a57c448ccd2d5
ari123aji <ari.purnama.aji26@gmail.com>
2021-11-16 17:27:51 +0700

    main

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -1,32 +1,54 @@
 @extends('template.main')
 
 @section('container')
-<h2 style="font-family: Phantomsans, sans-serif;text-align: center;" class="mt-3">
-Dokumentasi Musik yang Pernah Dipesan</h2>
 @if($orders->count())
-    <div class="card mt-4 mb-3">
-        @foreach($orders as $order)
-        <div style="max-height: 720px; overflow:hidden;">
-            <center><video width="640" height="480" controls>
-                <source src="{{ asset('storage/'.$order->video) }}" type="video/mp4">
+<div class="card mb-3">
+    <div style="max-height: 720px; overflow:hidden;">
+        <center><video width="640" height="480" controls>
+                <source src="{{ asset('storage/'.$orders[0]->video) }}" type="video/mp4">
                 Your browser does not support the video tag.
-            </video>
-            </center>
-        </div>
-        <div class="card-body text-center">
-            <h3 class="card-title">{{ $order->judul_lagu }}</h3>
-            <p>
-                <small> Dipesan oleh : {{ $order->author->unit_mb }}</a>
-                {{ $order->created_at->diffForHumans() }}
-                </small>
-            </p>
-        </div>
+            </video></center>
     </div>
-    @endforeach
+    <div class="card-body text-center">
+        <h3 class="card-title"><a href="" class="text-decoration-none text-dark">{{ $orders[0]->judul_lagu }}</a></h3>
+        <p>
+            <small> Dipesan oleh : {{ $orders[0]->author->unit_mb }}</a>
+                {{ $orders[0]->created_at->diffForHumans() }}
+            </small>
+        </p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        @foreach ($orders->skip(1) as $order)
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div style="max-height: 720px; overflow:hidden;">
+                    <center><video width="640" height="480" controls>
+                            <source src="{{ asset('storage/'.$order->video) }}" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </center>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $order->judul_lagu }}</h5>
+                    <p>
+                        <small> Dipesan oleh : {{ $order->author->unit_mb }}</a>
+                            {{ $order->created_at->diffForHumans() }}
+                        </small>
+                    </p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @else
-    <p class="text-center fs-4" >Post not Found...</p>
+<p class="text-center fs-4">Belum ada pesanan</p>
 @endif
-    <div class="d-flex justify-content-center">
+
+<div class="d-flex justify-content-center">
     {{ $orders->links() }}
-    </div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('template.main')
 
-@section('container')   
+@section('container')
 <div class="big-image2">
   <div class="overlay">
     <div class="text-box">
-        <h1 style="font-family: merienda;">Selamat Datang</h1>
-        <p style="font-family: 'Sofia', sans-serif; font-size: 15px;">Sistem Informasi Pemesanan Aransemen dan Notasi Musik Marching Band</p>
-            <a href="/login" class="button-64" role="button">Login</a> 
-            <a href="/register" class="button-64 mt-4" role="button">Registrasi</a>
+      <h1 style="font-family: merienda;">Selamat Datang</h1>
+      <p style="font-family: 'Sofia', sans-serif; font-size: 15px;">Sistem Informasi Pemesanan Aransemen dan Notasi Musik Marching Band</p>
+      <a href="/login" class="button-64" role="button">Login</a>
+      <a href="/register" class="button-64 mt-4" role="button">Registrasi</a>
     </div>
   </div>
-</div>     
-    <!-- <center>
+</div>
+<!-- <center>
         <div style="font-family: 'Sofia', sans-serif;">
             
             <h2>Sistem Informasi Pemesanan Aransemen dan Notasi Musik Marching Band</h2>
